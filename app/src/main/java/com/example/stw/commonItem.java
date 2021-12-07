d04e8da24650648452da6ca3a29cef7ac8ebea51
SuBin-Park1113 <wlqkr23>
2021-12-07 17:26:53 +0900

    Common Diary - ing 2

--- app/src/main/java/com/example/stw/commonItem.java
+++ app/src/main/java/com/example/stw/commonItem.java
@@ -3,4 +3,17 @@ package com.example.stw;
 public class commonItem {
 
     String title;
+
+    public commonItem(String title)
+    {
+        this.title=title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
 }
---
+++ app/src/main/java/com/example/stw/commonItemView.java
@@ -0,0 +1,34 @@
+package com.example.stw;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+public class commonItemView extends LinearLayout {
+
+    TextView textTitle;
+
+    public commonItemView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public commonItemView(Context context, @Nullable AttributeSet attrs){
+        super(context, attrs);
+    }
+
+    private void init (Context context){
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.common_item_list,this,true);
+
+        textTitle = findViewById(R.id.title);
+    }
+
+    public void setTitle(String title){
+        textTitle.setText(title);
+    }
+}
